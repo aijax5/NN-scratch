1e652aec65736a31cc9e8229047e14cb800f7581
aijax5 <mdajaaz1999@gmail.com>
2020-02-19 23:44:39 +0530

    set and getweights working, TODO: train on new

---
+++ split_learn.py
@@ -0,0 +1,15 @@
+import numpy as np
+from src.NN import NN
+import src.utils as utils
+# if(__name__ == 'main')
+# Settings
+csv_filename = "data/seeds_dataset.csv"
+hidden_layers = [1,2] # number of nodes in hidden layers i.e. [layer1, layer2, ...]
+eta = 0.1 # learning rate
+n_epochs = 50 # number of training epochs
+n_folds = 4 # number of folds for cross-validation
+seed_crossval = 1 # seed for cross-validation
+seed_weights = 1 # 
+def splitLearn(X,Y):
+
+if __name__ == "__main__" :
--- src/NN.py
+++ src/NN.py
@@ -8,17 +8,19 @@ import numpy as np
 
 class NN:
 
-    def __init__(self, input_dim=None, output_dim=None, hidden_layers=None, seed=1):
+    def __init__(self, input_dim=None, output_dim=None, hidden_layers=None,AvgWeights=None):
         if (input_dim is None) or (output_dim is None) or (hidden_layers is None):
             raise Exception("Invalid arguments given!")
         self.input_dim = input_dim # number of input nodes
         self.output_dim = output_dim # number of output nodes
         self.hidden_layers = hidden_layers # number of hidden nodes @ each layer
-        self.network = self._build_network(seed=seed)
+        self.network = self._build_network(AvgWeights)
+        # return self
 
+    # def __init__
     # Train network
     def train(self, X, y, eta=0.5, n_epochs=200):
-        for epoch in range(n_epochs):
+        for _ in range(n_epochs):
             for (x_, y_) in zip(X, y):
                 self._forward_pass(x_) # forward pass (update node["output"])
                 yhot_ = self._one_hot_encoding(y_, self.output_dim) # one-hot target
@@ -35,10 +37,12 @@ class NN:
         for layer in self.network:
             lw=list()
             for node in layer:
-                lw.append(np.asarray(node["weights"], dtype=np.float32))
-            w.append(np.asarray(lw, dtype=np.float32))
-        return np.array(w)
-        
+                print("node : ",node)
+                # if type()
+                lw.append(node["weights"])
+            w.append(lw)
+        return w
+    
     # ==============================
     #
     # Internal functions
@@ -46,14 +50,18 @@ class NN:
     # ==============================
 
     # Build fully-connected neural network (no bias terms)
-    def _build_network(self, seed=1):
+    def _build_network(self,AvgWeights=None):
         from datetime import datetime
         random.seed(datetime.now())
         # Create a single fully-connected layer
-        def _layer(input_dim, output_dim):
+        def _layer(input_dim, output_dim, Avgweight=None):
             layer = []
-            for i in range(output_dim):
-                weights = [random.random() for _ in range(input_dim)] # sample N(0,1)
+            for _ in range(output_dim):
+                if type(Avgweight) == np.ndarray:
+                    weights = Avgweight.tolist()
+                   
+                else :
+                    weights = [random.random() for _ in range(input_dim)] # sample N(0,1)
                 node = {"weights": weights, # list of weights
                         "output": None, # scalar
                         "delta": None} # scalar
@@ -61,16 +69,33 @@ class NN:
             return layer
 
         # Stack layers (input -> hidden -> output)
-        network = []
-        if len(self.hidden_layers) == 0:
-            network.append(_layer(self.input_dim, self.output_dim))
-        else:
-            network.append(_layer(self.input_dim, self.hidden_layers[0]))
-            for i in range(1, len(self.hidden_layers)):
-                network.append(_layer(self.hidden_layers[i-1], self.hidden_layers[i]))
-            network.append(_layer(self.hidden_layers[-1], self.output_dim))
+        if type(AvgWeights) == list:
+            network = []
+            if len(self.hidden_layers) == 0:
+                network.append(_layer(self.input_dim,self.output_dim,AvgWeights[0]))
 
-        return network
+            else:
+                network.append(_layer(self.input_dim, self.hidden_layers[0],AvgWeights[0]))
+                for i in range(1, len(self.hidden_layers)):
+                    network.append(_layer(self.hidden_layers[i-1], self.hidden_layers[i],AvgWeights[i]))
+                network.append(_layer(self.hidden_layers[-1], self.output_dim,AvgWeights[-1]))
+
+            return network
+            
+
+        else:
+            # pass
+            network = []
+            if len(self.hidden_layers) == 0:
+                network.append(_layer(self.input_dim, self.output_dim))
+            else:
+                network.append(_layer(self.input_dim, self.hidden_layers[0]))
+                for i in range(1, len(self.hidden_layers)):
+                    network.append(_layer(self.hidden_layers[i-1], self.hidden_layers[i]))
+                network.append(_layer(self.hidden_layers[-1], self.output_dim))
+
+            return network
+            
 
     # Forward-pass (updates node['output'])
     def _forward_pass(self, x):
